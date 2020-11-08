154387640cef4033e524a099bba9cb9a22b42b0b
sajetan <sajetnayaran@gmail.com>
2020-11-08 04:24:42 +0100

    jee2 assignment files

---
+++ carRentalLabSessions_v4/CarRental-client/src/java/client/Main.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.util.Date;
+import java.util.List;
+import javax.naming.InitialContext;
+import rental.CarType;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import session.ManagerSessionRemote;
+import session.ReservationSessionRemote;
+
+public class Main extends AbstractTestManagement<ReservationSessionRemote, ManagerSessionRemote> {
+
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO: use updated manager interface to load cars into companies
+        new Main("trips").run();
+    }
+}
